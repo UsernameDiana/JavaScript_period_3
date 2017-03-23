46173fd5e36cc7563a9e4efbd46391096cf40414
Diana <diana@Dianas-MacBook-Air.local>
2017-03-23 13:59:10 +0100

    connection

--- mongooseExercise/models/Jokes.js
+++ mongooseExercise/models/Jokes.js
@@ -1,13 +1,19 @@
 'use strict'
+var connection = require("../db/db");
+var db = connection.get();  //Get it like this
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
-var connect = require("../db/mongooseConnect")
-
 
 var JokeSchema = new Schema( // define schema
     {
-        "joke": " Reality is an illusion created by a lack of alcohol",
         "joke": "I intend to live forever, or die Klausen",
+        joke: { type: String, required: true, minLength: 10 }, // joke property, minimum accepted length is 5;
+        "category": ["short", "alcohol", "quote"],
+        "reference": { "author": "Someone", "link": "" },
+        "lastEdited": { type: Date, default: Date.now }
+    },
+    {
+        "joke": " Reality is an illusion created by a lack of alcohol",
         joke: { type: String, required: true, minLength: 10 }, // joke property, minimum accepted length is 5;
         "category": ["short", "alcohol", "quote"],
         "reference": { "author": "Someone", "link": "" },
@@ -21,24 +27,24 @@ JokeSchema.pre('update', function (next) {
 
 // This will be façade to the database
 // function(err,data){..} data is joke or jokes depending on question
-function allJokes (callback) {
-var db = connect.JokeSchema
+function allJokes(callback) {
+    var db = connect.JokeSchema
 };
-function findJoke (id, callback) {
+function findJoke(id, callback) {
 
 };
-function addJoke (jokeToAdd, callback) {
-    
+function addJoke(jokeToAdd, callback) {
+
 };
-function editJoke (jokeToEdit, callback) { };
-function deleteJoke (id, callback) { };
+function editJoke(jokeToEdit, callback) { };
+function deleteJoke(id, callback) { };
 exports.randomJoke = function randomJoke(callback) { };
 
 // exporting functions for use in api/api.js
 module.exports = {
-allJokes: allJokes,
-findJoke: findJoke,
-addJoke: addJoke,
-editJoke: editJoke,
-deleteJoke: deleteJoke
+    allJokes: allJokes,
+    findJoke: findJoke,
+    addJoke: addJoke,
+    editJoke: editJoke,
+    deleteJoke: deleteJoke
 };
