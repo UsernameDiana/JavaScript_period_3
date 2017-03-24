05efc69cf2c37704e86db9343636a5b98b4eac55
Diana <diana@Dianas-MacBook-Air.local>
2017-03-24 13:41:57 +0100

    Jokes schema cleaned up

--- mongooseExercise/models/Jokes.js
+++ mongooseExercise/models/Jokes.js
@@ -1,51 +1,19 @@
 'use strict'
-var connection = require("../db/db");
-var db = connection.get();  //Get it like this
 
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 var JokeSchema = new Schema( // define schema
     {
-        "joke": "I intend to live forever, or die Klausen",
-        joke: { type: String, required: true, minLength: 10 }, // joke property, minimum accepted length is 5;
-        "category": ["short", "alcohol", "quote"],
-        "reference": { "author": "Someone", "link": "" },
-        "lastEdited": { type: Date, default: Date.now }
-    },
-    {
-        "joke": " Reality is an illusion created by a lack of alcohol",
-        joke: { type: String, required: true, minLength: 10 }, // joke property, minimum accepted length is 5;
-        "category": ["short", "alcohol", "quote"],
-        "reference": { "author": "Someone", "link": "" },
-        "lastEdited": { type: Date, default: Date.now }
+        joke: { type: String, required: true, minLength: 5 }, // joke property, minimum accepted length is 5;
+        category: [String],
+        reference: { author: String, link: String },
+        lastEdited: { type: Date, default: Date.now }
     });
 
-JokeSchema.pre('update', function (next) {
+JokeSchema.pre('save', function (next) {
     this.lastEdited = new Date();
     next();
 });
 
-// This will be façade to the database
-// function(err,data){..} data is joke or jokes depending on question
-function allJokes(callback) {
-    var db = connect.JokeSchema
-};
-function findJoke(id, callback) {
-
-};
-function addJoke(jokeToAdd, callback) {
-
-};
-function editJoke(jokeToEdit, callback) { };
-function deleteJoke(id, callback) { };
-exports.randomJoke = function randomJoke(callback) { };
-
-// exporting functions for use in api/api.js
-module.exports = {
-    allJokes: allJokes,
-    findJoke: findJoke,
-    addJoke: addJoke,
-    editJoke: editJoke,
-    deleteJoke: deleteJoke
-};
+mongoose.model("Joke", JokeSchema);
